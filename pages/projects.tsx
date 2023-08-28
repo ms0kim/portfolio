9f484d8cd1bd2b7792a451fc2eba3da2aa8e0ceb
Sjuwon <102498237+Sjuwon@users.noreply.github.com>
2023-08-28 14:37:30 +0900

    최종

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,13 @@
 import HeadComponent from "./component/Head"
 import ProjectsPage from "./component/Projects"
+import Cursor from "./component/Cursor"
 
 export default function Projects() {
   return (
     <>
       <HeadComponent />
       <ProjectsPage />
+      <Cursor />
     </>
   )
 }

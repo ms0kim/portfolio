7d1f4d8462a7c1f14f8f5fc0b43f63a7d4f6e27e
Sjuwon <102498237+Sjuwon@users.noreply.github.com>
2023-08-28 22:20:29 +0900

    최종

--- pages/component/Home/index.tsx
+++ pages/component/Home/index.tsx
@@ -8,7 +8,6 @@ import Footer from "../Footer";
 
 export default function HomePage() {
   const [hide, setHide] = useState(false);
-  const [touchStartY, setTouchStartY] = useState(null);
 
   const handleOnWheel = (e:any) => {
     if (e.nativeEvent.wheelDelta > 0) {
@@ -19,30 +18,8 @@ export default function HomePage() {
       setHide(true);
     }
   }
-  const handleTouchStart = (e:any) => {
-    setTouchStartY(e.touches[0].clientY);
-  }
-  const handleTouchMove = (e:any) => {
-    if (touchStartY !== null) {
-      const deltaY = e.touches[0].clientY - touchStartY;
-
-      if (deltaY > 10) {
-        setHide(true);
-      } else if (deltaY < -10) {
-        setHide(false);
-      }
-
-      setTouchStartY(null);
-    }
-  }
 
   useEffect(() => { //마운트 return하면 언마운트
-    window.addEventListener("touchstart", handleTouchStart);
-    window.addEventListener("touchmove", handleTouchMove);
-    return () => {
-      window.removeEventListener("touchstart", handleTouchStart);
-      window.removeEventListener("touchmove", handleTouchMove);
-    };
   },[]);
 
   return (

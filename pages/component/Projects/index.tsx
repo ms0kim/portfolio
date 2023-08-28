0892f9ed31d1f0963eb67ebf722c12dc813b5660
Sjuwon <102498237+Sjuwon@users.noreply.github.com>
2023-08-28 22:15:01 +0900

    최종

--- pages/component/Projects/index.tsx
+++ pages/component/Projects/index.tsx
@@ -6,6 +6,8 @@ import Footer from "../Footer";
 
 export default function ProjectsPage() {
   const [hide, setHide] = useState(false);
+  const [touchStartY, setTouchStartY] = useState(null);
+
   const handleOnWheel = (e:any) => {
     if (e.nativeEvent.wheelDelta > 0) {
       // scroll up event
@@ -15,17 +17,28 @@ export default function ProjectsPage() {
       setHide(true);
     }
   }
+  const handleTouchStart = (e:any) => {
+    setTouchStartY(e.touches[0].clientY);
+  }
   const handleTouchMove = (e:any) => {
-    if (e.touches[0].clientY > 0) {
-      setHide(true);
-    } else {
-      setHide(false);
+    if (touchStartY !== null) {
+      const deltaY = e.touches[0].clientY - touchStartY;
+
+      if (deltaY > 10) {
+        setHide(true);
+      } else if (deltaY < -10) {
+        setHide(false);
+      }
+
+      setTouchStartY(null);
     }
   }
 
   useEffect(() => {
+    window.addEventListener("touchstart", handleTouchStart);
     window.addEventListener("touchmove", handleTouchMove);
     return () => {
+      window.removeEventListener("touchstart", handleTouchStart);
       window.removeEventListener("touchmove", handleTouchMove);
     };
   },[]);
